9bb9858b74f31258aa29ec6d990a2330f55f41c3
Guy Carlos Villette <carlosvillette@gmail.com>
2025-01-10 21:57:59 -0500

    Use our HCP Terraform organization fix

--- main.tf
+++ main.tf
@@ -21,10 +21,10 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "balericaclass6"
+    organization = "dudepablo1"
 
     workspaces {
-      name = "learn-terraform-github-actions"
+      name = "TFE"
     }
   }
 }
